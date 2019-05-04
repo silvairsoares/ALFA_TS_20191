61a952dac81165441eef5b7890b017fde6102f98
Silvair L. Soares <silvairsoares@outlook.com>
2019-05-04 08:19:59 -0300

    Atualização dia 27/04/19
    Atualização dia 27/04/19

--- avaliacao-Escolar/src/test/java/AvaliacaoTeste.java
+++ avaliacao-Escolar/src/test/java/AvaliacaoTeste.java
@@ -1,13 +1,14 @@
-import br.com.gilmar.Avaliacao;
-import br.com.gilmar.ValoresInvalidosException;
+package br.com.gilmar;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertTrue;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class AvaliacaoTeste {
-
-    private Avaliacao avaliacao;
+	private Avaliacao avaliacao;
 
     @Before
     public void setup(){
@@ -25,9 +26,9 @@ public class AvaliacaoTeste {
     public void testaQuantidadeParametrosMaiorQuatro(){
         String resultado = avaliacao.avalia(7.0, 6.0, 5, 96, 48);
     }
- */
+*/
 
-    //CT03
+  //CT03
     @Test
     public void testaNota1MenorZero(){
         try{
@@ -47,5 +48,109 @@ public class AvaliacaoTeste {
             Assert.assertTrue(e instanceof ValoresInvalidosException);
         }
 
+    }
+
+    //CT05
+    @Test
+    public void testaNota2MenorZero(){
+        try{
+            String resultado = avaliacao.avalia(8.0, -1.0, 5.0,96.0);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT06
+    @Test
+    public void testaNota2MaiorDez(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 11.0, 5,96);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT07
+    @Test
+    public void testaQuantidadeFaltasMenorZero(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 7.0, -3,96);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT08
+    @Test
+    public void testaQuantidadeFaltasMaiorCargaHoraria(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 7.0, 97, 96);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT09
+    @Test
+    public void testaCargaHorariaMenorZero(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 7.0, 16, -1);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT10
+    @Test
+    public void testaCargaHorariaMaior97(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 7.0, 16, 97);
+        }catch (Exception e){
+            Assert.assertTrue(e instanceof ValoresInvalidosException);
+        }
+    }
+
+    //CT11
+    @Test
+    public void testaReprovacaoPorFalta(){
+        try{
+            String resultado = avaliacao.avalia(8.0, 7.0, 28, 96);
+            Assert.assertTrue(resultado.equals("Reprovado por Falta."));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //CT12
+    @Test
+    public void testaReprovacaoPorMedia(){
+        try{
+            String resultado = avaliacao.avalia(2.0, 3.0, 15, 96);
+            Assert.assertTrue(resultado.equals("Reprovado por Média."));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //CT13
+    @Test
+    public void testaSegundaChamada(){
+        try{
+            String resultado = avaliacao.avalia(4.0, 5.0, 15, 96);
+            Assert.assertTrue(resultado.equals("Prova Extra."));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //CT14
+    @Test
+    public void testaAprovacao(){
+        try{
+            String resultado = avaliacao.avalia(7.0, 8.0, 15, 96);
+            Assert.assertTrue(resultado.equals("Aprovado."));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
